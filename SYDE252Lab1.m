b80152e3efcf8c34387d4cc289f3fd9adb3d779d
Jeff Jun <Artemis@v1020-wn-189-168.campus-dynamic.uwaterloo.ca>
2015-10-22 14:15:04 -0400

    blah

--- SYDE252Lab1.m
+++ SYDE252Lab1.m
@@ -2,17 +2,22 @@ clear all;
 clc;
 clear all;
 
-[y,fs]=audioread('drumloop1.wav');
+[y,fs]=audioread('ClayColoredRobin.wav');
 y = y(:,1);
 dt = 1/fs;
 t = 0:dt:(length(y)*dt)-dt;
 
 %plot(t,y);
-win_3 = win_average(3, y);
-win_5 = win_average(5, y);
-win_7 = win_average(7, y);
-plot(t,y,'b', t,win_3,'r', t,win_5,'y', t,win_7,'g');
-figure;
-plot(t,y,t,abs(y));
+% win_3 = win_average(3, y);
+% win_5 = win_average(4, y);
+% win_7 = win_average(7, y);
+% plot(t,y,'b', t,win_5,'y');
 
-audiowrite('drumloop1_new1.wav',win_7,fs);
+figure;
+gauss_filt20 = gauss_filt(4000, y);
+plot(t,y,t,gauss_filt20);
+
+figure;
+G = fspecial('gaussian', 20, 1);
+builtgauss = conv(y,G,'same');
+plot(t,y,t,abs(y));
--- gauss_filt.m
+++ gauss_filt.m
@@ -1,5 +1,8 @@
 function result = gauss_filt(WIN, sig_val)
     sigma = 1;
-    size = 5;
-    
+    size = WIN;
+    x = linspace(-size / 2, size / 2, size);
+    gaussFilter = exp(-x.^2 / (2*sigma^2));
+    gaussFilter = gaussFilter / sum( gaussFilter );
+    result = conv( sig_val, gaussFilter, 'same');
 end
